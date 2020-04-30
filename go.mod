65034994258fb4b8a78380ca5c7dbb48a74d2d90
mr5 <123@123.com>
2020-04-30 17:00:01 +0800

    v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wallemr5/gmtest
+module github.com/wallemr5/gmtest/v2
 
 go 1.14
--- sums.go
+++ sums.go
@@ -1,8 +1,11 @@
-package gmtest
+package gotest
 
 import "fmt"
 
 func Add(i,j int) int  {
 	fmt.Println("hellow world")
 	return i+j
+}
+func Show()  {
+	fmt.Println("HELLO WORLD..............")
 }
